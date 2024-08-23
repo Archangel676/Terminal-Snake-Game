081efc5db7c8e45b61bde151e07d8fc8ef4bcb04
Maya <mvenega9@my.chemeketa.edu>
2024-08-23 02:43:34 +0000

    Aug 22, 2024, 7:43 PM

--- main.cpp
+++ main.cpp
@@ -11,6 +11,48 @@ int snakeX, snakeY, fruitX, fruitY, score;
 enum direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
 direction dir;
 
+class Renderer {
+public:
+  virtual void RenderSnake(int x, int y) = 0;
+  virtual void RenderFruit(int x, int y) = 0;
+  virtual void RenderBoard(int width, int height) = 0;
+  virtual ~Renderer()
+  {
+  }
+};
+
+class TextRenderer: public Renderer {
+public:
+  void RenderSnake(int x, int y) override
+  {
+    // Fill in the text-based rendering code
+  }
+  void RenderFruit(int x, int y) override
+  {
+    // Fill in the text-based rendering code
+  }
+  void RenderBoard(int width, int height) override
+  {
+    // Fill in the text-based rendering code
+  }
+};
+
+class ImageRenderer: public Renderer {
+public:
+  void RenderSnake(int x, int y) override
+  {
+    // Fill in the image-based rendering code
+  }
+  void RenderFruit(int x, int y) override
+  {
+    // Fill in the image-based rendering code
+  }
+  void RenderBoard(int width, int height) override
+  {
+    // Fill in the image-based rendering code
+  }
+};
+
 void Setup()
 {
   gameOver = false;
