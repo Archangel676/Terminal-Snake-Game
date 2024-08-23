85c16a5691fe4b268cd32df36469499f706eb4e7
Maya <mvenega9@my.chemeketa.edu>
2024-08-23 01:44:04 +0000

    Aug 22, 2024, 6:44 PM

--- main.cpp
+++ main.cpp
@@ -2,14 +2,44 @@
 using namespace std;
 
 bool gameOver;
+const int width = 20;
+const int height = 20;
+int snakeX, snakeY, fruitX, fruitY, score;
+enum direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+direction dir;
 
 void Setup()
 {
   gameOver = false;
+  dir = STOP;
+  snakeX = width / 2;
+  snakeY = height / 2;
+  fruitX = rand() % width;
+  fruitY = rand() % height;
+  score = 0;
 }
 
 void Draw()
 {
+  system("cls");
+  for (int i = 0; i < width; i++)
+    cout << "#";
+
+  cout << endl;
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      if (j == 0 || j == width - 1)
+        cout << "#";
+      else
+        cout << " ";
+    }
+    cout << endl;
+  }
+
+  for (int i = 0; i < width; i++)
+    cout << "#";
+  cout << endl;
 }
 
 void Input()
@@ -27,6 +57,7 @@ int main()
     Draw();
     Input();
     Logic();
+    // Sleep(10);
   }
 
   return 0;
