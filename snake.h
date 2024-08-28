f367a2dc7151bae64f5328f9b746b13fe61511a6
Maya <mvenega9@my.chemeketa.edu>
2024-08-28 22:10:56 +0000

    Aug 28, 2024, 3:10 PM

---
+++ snake.h
@@ -0,0 +1,27 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+class TicTacToe {
+private:
+  // char currentPlayer;
+  // char board[3][3];
+
+public:
+  /**
+   * @brief Constructs a new Tic Tac Toe object.
+   *        Sets default starting player to X and
+   *        initializes board with dash '-' characters
+
+  TicTacToe();*/
+
+  /**
+   * @brief Loops through the two dimensional array
+   *        of the current board and turns it into a string
+   *        that if printed to the console, will be formatted
+   *        like a regular Tic Tac Toe game
+   * @return std::string String containing current game log
+
+  std::string toString(); */
+};
+
+#endif // SNAKE_H
