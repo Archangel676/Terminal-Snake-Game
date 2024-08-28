e571448d4cd26c2f1c48630cc21afa5f36959e11
Maya <mvenega9@my.chemeketa.edu>
2024-08-28 22:23:39 +0000

    Aug 28, 2024, 3:23 PM

--- game.cpp
+++ game.cpp
@@ -1,2 +1,16 @@
 #include "game.h"
 #include <iostream>
+
+// sf::RenderWindow window {}
+
+// sf::Texture snakeHeadTexture {}
+// sf::Texture snakeBodyTexture {}
+// sf::Texture snakeCornerTexture {}
+// sf::Texture snakeTailTexture {}
+// sf::Texture fruitTexture {}
+
+// sf::Sprite snakeHeadSprite {}
+// sf::Sprite snakeBodySprite {}
+// sf::Sprite snakeCornerTexture {}
+// sf::Sprite snakeTailTexture {}
+// sf::Sprite fruitSprite {}
